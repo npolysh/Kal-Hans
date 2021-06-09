948f768d814059c49f505bde7d726f3b584f76c8
Nina Polish <72516109+npolysh@users.noreply.github.com>
2021-06-09 17:12:42 +0300

    ADD: reviews section

---
+++ swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 4.5,
+    spaceBetween: 20,
+    loop: true,
+    freeMode: true,
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+  
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    mousewheel: true,
+  });
