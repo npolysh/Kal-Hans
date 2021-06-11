f196b99a0487e20b9bddce8085b3c855e6de98f1
Nina Polish <72516109+npolysh@users.noreply.github.com>
2021-06-11 17:04:40 +0300

    Add: footer section

---
+++ script.js
@@ -0,0 +1,14 @@
+// let decrease = document.getElementById('decrease');
+// let increase = document.getElementById('increase');
+// let countResult = document.getElementById('count-result');
+// countResult.value = 1;
+
+// decrease.addEventListener = ('click', () => {
+//     countResult.value--
+// });
+
+// increase.addEventListener = ('click', () => {
+//     countResult.value++
+// });
+
+// countResult.append = countResult.value;
